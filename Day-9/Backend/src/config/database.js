eb8c1b6a18867f66a6485eee77117c55a2fd24e2
Jelaance0o <162440781+Jelaance0o@users.noreply.github.com>
2026-09-15 13:16:03 +0530

    ImageKit Error

--- Day-9/Backend/src/config/database.js
+++
@@ -1,8 +0,0 @@
-const { default: mongoose } = require("mongoose")
-
-function connectToDB(){
-    mongoose.connect(process.env.MONGODB_URI).then(() => {
-      console.log("Database Connected");
-    });
-}
-module.exports = connectToDB
